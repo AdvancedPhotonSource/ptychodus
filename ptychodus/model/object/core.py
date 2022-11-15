9187903a23e044ca736a15f2560e79f0595c9c27
Steven Henke <shenke@anl.gov>
2022-11-14 21:55:37 -0600

    clean up controller.object module

--- ptychodus/model/object/core.py
+++ ptychodus/model/object/core.py
@@ -127,6 +127,25 @@ class ObjectPresenter(Observable, Observer):
 
 class ObjectCore:
 
+    @staticmethod
+    def _createInitializerChooser(
+            rng: numpy.random.Generator, settings: ObjectSettings, sizer: ObjectSizer,
+            fileReaderChooser: PluginChooser[ObjectFileReader]
+    ) -> PluginChooser[ObjectInitializer]:
+        initializerList = [
+            FileObjectInitializer.createInstance(settings, sizer, fileReaderChooser),
+            UniformObjectInitializer.createInstance(settings, sizer),
+            UniformRandomObjectInitializer(rng, sizer),
+        ]
+
+        pluginList = [
+            PluginEntry[ObjectInitializer](simpleName=ini.simpleName,
+                                           displayName=ini.displayName,
+                                           strategy=ini) for ini in initializerList
+        ]
+
+        return PluginChooser[ObjectInitializer].createFromList(pluginList)
+
     def __init__(self, rng: numpy.random.Generator, settingsRegistry: SettingsRegistry,
                  apparatus: Apparatus, scan: Scan, probeSizer: ProbeSizer,
                  fileReaderChooser: PluginChooser[ObjectFileReader],
@@ -135,24 +154,8 @@ class ObjectCore:
         self.sizer = ObjectSizer.createInstance(apparatus, scan, probeSizer)
         self.object = Object(self.sizer)
 
-        self._filePlugin = PluginEntry[ObjectInitializer](
-            simpleName='FromFile',
-            displayName='Open File...',
-            strategy=FileObjectInitializer.createInstance(self.settings, self.sizer,
-                                                          fileReaderChooser),
-        )
-        self._uniformPlugin = PluginEntry[ObjectInitializer](
-            simpleName='Uniform',
-            displayName='Uniform',
-            strategy=UniformObjectInitializer.createInstance(self.settings, self.sizer),
-        )
-        self._urandPlugin = PluginEntry[ObjectInitializer](
-            simpleName='Random',
-            displayName='Random',
-            strategy=UniformRandomObjectInitializer(rng, self.sizer),
-        )
-        self._initializerChooser = PluginChooser[ObjectInitializer].createFromList(
-            [self._filePlugin, self._uniformPlugin, self._urandPlugin])
+        self._initializerChooser = ObjectCore._createInitializerChooser(
+            rng, self.settings, self.sizer, fileReaderChooser)
 
         self.presenter = ObjectPresenter.createInstance(self.settings, self.sizer, apparatus,
                                                         self.object, self._initializerChooser,
