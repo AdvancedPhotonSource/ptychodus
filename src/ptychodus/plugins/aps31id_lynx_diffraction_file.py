00b4e7ea2fd7405c14d596594bb1c9221c791abf
Steven Henke <shenke@anl.gov>
2025-07-10 07:28:53 -0500

    add metadata to lynx diffraction file reader

--- src/ptychodus/plugins/aps31id_lynx_diffraction_file.py
+++ src/ptychodus/plugins/aps31id_lynx_diffraction_file.py
@@ -6,6 +6,7 @@ import numpy
 
 from ptychodus.api.geometry import ImageExtent, PixelGeometry
 from ptychodus.api.patterns import (
+    CropCenter,
     DiffractionDataset,
     DiffractionFileReader,
     DiffractionMetadata,
@@ -26,32 +27,70 @@ class LYNXDiffractionFileReader(DiffractionFileReader):
     def read(self, file_path: Path) -> DiffractionDataset:
         with h5py.File(file_path, 'r') as h5_file:
             contents_tree = self._tree_builder.build(h5_file)
+            data = h5_file[self._data_path]
+
+            if not isinstance(data, h5py.Dataset):
+                raise KeyError(f'{self._data_path} is not a Dataset!')
+
+            num_patterns, detector_height, detector_width = data.shape
+
+            crop_center: CropCenter | None = None
+
+            count_cutoff: int | None = None
+            detector_distance_m: float | None = None
+            exposure_period: float | None = None
+            exposure_time: float | None = None
+            probe_energy_eV: float | None = None  # noqa: N806
+            detector_pixel_geometry: PixelGeometry | None = None
+            silicon: float | None = None
+            tau: float | None = None
+            image_nr_high: int | None = None
+            image_nr_low: int | None = None
 
             try:
-                data = h5_file[self._data_path]
-                pixel_size = float(data.attrs['Pixel_size'].item())
+                center_x_px: int = data.attrs['Center_x_pixel'].item()
+                center_y_px: int = data.attrs['Center_y_pixel'].item()
+                count_cutoff = data.attrs['Count_cutoff'].item()
+                detector_distance_m = data.attrs['Detector_distance_m'].item()
+                # FIXME exposure period/time to metadata
+                exposure_period = data.attrs['Exposure_period'].item()
+                exposure_time = data.attrs['Exposure_time'].item()
+                photon_energy_keV = data.attrs['Photon_energy_kev'].item()  # noqa: N806
+                pixel_size = data.attrs['Pixel_size'].item()
+                silicon = data.attrs['Silicon'].item()
+                tau = data.attrs['Tau'].item()
+                image_nr_high = data.attrs['image_nr_high'].item()
+                image_nr_low = data.attrs['image_nr_low'].item()
             except KeyError:
-                logger.warning('Unable to load data.')
+                pass
             else:
-                num_patterns, detector_height, detector_width = data.shape
+                detector_pixel_geometry = PixelGeometry(pixel_size, pixel_size)
+                crop_center = CropCenter(center_x_px, center_y_px)
+                probe_energy_eV = 1000.0 * photon_energy_keV  # noqa: N806
 
-                metadata = DiffractionMetadata(
-                    num_patterns_per_array=num_patterns,
-                    num_patterns_total=num_patterns,
-                    pattern_dtype=data.dtype,
-                    detector_extent=ImageExtent(detector_width, detector_height),
-                    detector_pixel_geometry=PixelGeometry(pixel_size, pixel_size),
-                    file_path=file_path,
-                )
+            metadata = DiffractionMetadata(
+                num_patterns_per_array=num_patterns,
+                num_patterns_total=num_patterns,
+                pattern_dtype=data.dtype,
+                detector_distance_m=detector_distance_m,
+                detector_extent=ImageExtent(detector_width, detector_height),
+                detector_pixel_geometry=detector_pixel_geometry,
+                # FIXME detector_bit_depth: int | None = None
+                crop_center=crop_center,
+                # FIXME probe_photon_count: int | None = None
+                probe_energy_eV=probe_energy_eV,
+                # FIXME tomography_angle_deg: float | None = None
+                file_path=file_path,
+            )
 
-                array = H5DiffractionPatternArray(
-                    label=file_path.stem,
-                    indexes=numpy.arange(num_patterns),
-                    file_path=file_path,
-                    data_path=self._data_path,
-                )
+            array = H5DiffractionPatternArray(
+                label=file_path.stem,
+                indexes=numpy.arange(num_patterns),
+                file_path=file_path,
+                data_path=self._data_path,
+            )
 
-                return SimpleDiffractionDataset(metadata, contents_tree, [array])
+            return SimpleDiffractionDataset(metadata, contents_tree, [array])
 
         return SimpleDiffractionDataset.create_null(file_path)
 
